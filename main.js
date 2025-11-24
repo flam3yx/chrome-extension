250eba32f21f1e0223753be63151aa8d29c1ef41
flam3yx <154548576+flam3yx@users.noreply.github.com>
2025-11-24 01:26:44 +0100

    Add initial HTML, CSS, and JavaScript files for chrome extension

---
+++ main.js
@@ -0,0 +1,46 @@
+// function saveLead() {
+//     console.log("Button clicked from onclick attribute");
+// }
+
+/*******************************************************************/
+
+let myLeads = []; // array
+const inputEl = document.getElementById("input-el"); // const can't be reassigned
+let inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    ulEl.innerHTML += "<li>" + inputEl.value + "</li>";
+    inputEl.value = "";
+})
+
+// less efficient but working ↓
+// inputBtn.addEventListener("click", function () {
+//     console.log("Button clicked from addEventListener");
+
+//     myLeads.push(inputEl.value); // add the element at the end of the array
+//    // clear out the input field
+//     inputEl.value = "";
+//     console.log(myLeads);
+
+//     renderLeads();
+// });
+
+// function renderLeads() {
+//     let listItems = "";
+
+//     for (let i = 0; i < myLeads.length; i++) {
+//         listItems += "<li>" + myLeads[i] + "</li>";
+
+//         // Other methos ↓
+//         // create element
+//         // set text content
+//         // append to ul
+//         // const li = document.createElement("li");
+//         // li.textContent = myLeads[i];
+//         // ulEl.append(li);
+//     }
+
+//     ulEl.innerHTML = listItems;
+// }
