2d21df1e47c76a6ee9ed5c1df3b8208a62972b46
flam3yx <154548576+flam3yx@users.noreply.github.com>
2025-11-25 00:23:30 +0100

    Refactor lead saving logic and improve render function for better efficiency

--- main.js
+++ main.js
@@ -1,53 +1,33 @@
-// function saveLead() {
-//     console.log("Button clicked from onclick attribute");
-// }
-// add onclick in the html to work
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-/*******************************************************************/
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 
-let myLeads = []; // array
-const inputEl = document.getElementById("input-el"); // const can't be reassigned
-let inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    ulEl.innerHTML += `
-        <li>
-            <a target='_blank' href='${inputEl.value}'>
-                ${inputEl.value}
-            </a>
-        </li>
-    `;
-    inputEl.value = "";
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
 })
 
-// less efficient but working ↓
-// inputBtn.addEventListener("click", function () {
-//     console.log("Button clicked from addEventListener");
-
-//     myLeads.push(inputEl.value); // add the element at the end of the array
-//    // clear out the input field
-//     inputEl.value = "";
-//     console.log(myLeads);
-
-//     renderLeads();
-// });
-
-// function renderLeads() {
-//     let listItems = "";
-
-//     for (let i = 0; i < myLeads.length; i++) {
-//         listItems += "<li>" + myLeads[i] + "</li>";
-
-//         // Other methos ↓
-//         // create element
-//         // set text content
-//         // append to ul
-//         // const li = document.createElement("li");
-//         // li.textContent = myLeads[i];
-//         // ulEl.append(li);
-//     }
-
-//     ulEl.innerHTML = listItems;
-// }
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
