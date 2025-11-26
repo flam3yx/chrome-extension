805a61cf368abac09b86887c3a457e76654b1dbe
flam3yx <154548576+flam3yx@users.noreply.github.com>
2025-11-26 13:08:19 +0100

    Add tab saving functionality and improve button styles

--- main.js
+++ main.js
@@ -4,18 +4,30 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteAllBtn = document.getElementById("deleteAll-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({
+        active: true /* active tab */,
+        currentWindow: true /* active window */
+    }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
+                <a target="_blank" href="${leads[i]}">
                     ${leads[i]}
                 </a>
             </li>
@@ -26,18 +38,18 @@ function render(leads) {
 
 deleteAllBtn.addEventListener("dblclick", function () {
     localStorage.clear()
-    myLeads = [] //reassigned
-    renderLeads()
+    myLeads = []
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function () {
     if (inputEl.value === "") {
-        alert("Input not valid!").
+        alert("Input not valid!")
         return
-    } else {
-        myLeads.push(inputEl.value)
-        inputEl.value = ""
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLeads()
     }
+
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
